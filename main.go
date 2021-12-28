4601ab6fadd51c32733ef85ee48d2f25f79c3c52
ziqiang.zhang <cquptzzq@163.com>
2021-12-28 16:42:18 +0800

    add engine

--- main.go
+++ main.go
@@ -6,34 +6,43 @@ import (
 	"net/http"
 )
 
-func main(){
-	// func (mux *ServeMux) HandleFunc(pattern string, handler func(ResponseWriter, *Request)) {}
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	// ListenAndServe listens on the TCP network address addr and then calls
-	// Serve with handler to handle requests on incoming connections.
-	// 参数handler是一个接口，需要实现方法 ServeHTTP() ，也就是说，只要传入任何实现了 ServerHTTP 接口的实例，所有的HTTP请求，就都交给了该实例处理了。
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-}
+// Engine is the uni handler for all requests
+type Engine struct{}
 
 // type HandlerFunc func(ResponseWriter, *Request)
-func indexHandler(w http.ResponseWriter, r *http.Request){
-	// Fprintf formats according to a format specifier and writes to w.
-	// It returns the number of bytes written and any write error encountered.
-	_, err := fmt.Fprintf(w, "URL.Path = %q \n", r.URL.Path)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request){
-	for k,v := range r.Header {
-		_, err := fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+// 参数 ResponseWriter 可以构造针对请求 Request 的响应。
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) ()  {
+	// 路由
+	switch r.URL.Path {
+	case "/":
+		_, err := fmt.Fprintf(w, "URL.Path = %q \n", r.URL.Path)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
+	case "/hello":
+		for k,v := range r.Header {
+			_, err := fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		}
+	default:
+		_, err := fmt.Fprintf(w, "404 NOT FOUND: %s \n", r.URL.Path)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+}
+
+func main(){
+	// Engine type 实现了接口 Handler，就可以作为实例传入ListenAndServe中了
+	engine := new(Engine)
+	// ListenAndServe listens on the TCP network address addr and then calls
+	// Serve with handler to handle requests on incoming connections.
+	// 参数handler是一个接口，需要实现方法 ServeHTTP() ，也就是说，只要传入任何实现了 ServerHTTP 接口的实例，所有的HTTP请求，就都交给该实例处理了。
+	// engine实例拦截了所有的HTTP请求，请求有了统一的控制入口，就可以在实例中自定义路由、中间件、日志、异常处理等
+	err := http.ListenAndServe(":80", engine)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 }
